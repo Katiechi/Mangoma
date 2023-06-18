5bbb464655f424a7984b82233dc7ef5b3c5f30cb
Brian <brian.katiechi9@gmail.com>
2023-06-18 20:59:03 +0300

    Built the player functionality

--- hooks/useOnPlay.ts
+++ hooks/useOnPlay.ts
@@ -7,18 +7,16 @@ import { useUser } from "./useUser";
 
 const useOnPlay = (songs: Song[]) => {
   const player = usePlayer();
-  const subscribeModal = useSubscribeModal();
+ 
   const authModal = useAuthModal();
-  const { subscription, user } = useUser();
+  const {  user } = useUser();
 
   const onPlay = (id: string) => {
     if (!user) {
       return authModal.onOpen();
     }
 
-    if (!subscription) {
-      return subscribeModal.onOpen();
-    }
+  
 
     player.setId(id);
     player.setIds(songs.map((song) => song.id));
